10975a0710fa5d13cf7deb236703da17c11863b4
ae <inv2004@gmail.com>
2020-04-10 23:28:12 +0300

    2

--- src/knucleotide_nim.nim
+++ src/knucleotide_nim.nim
@@ -1,14 +1,22 @@
+import sequtils
 import strutils
+import tables
 
-proc getSeq(key: TaintedString): string =
+proc encode(c: char): byte =
+  (c.uint8 and 0b110'u8) shr 1
+
+proc genFreq(input: seq[byte], frame: uint8): CountTable =
+  dispose
+
+proc getSeq(key: TaintedString): seq[byte] =
   let file = "knucleotide-input.txt".open()
   var line: TaintedString
   while file.readLine(line):
     if line.startsWith(key):
       break
-  
+
   while file.readLine(line):
-    result.add(line)
+    result.add(line.mapIt(encode(it)))
 
 
 proc calc() =
